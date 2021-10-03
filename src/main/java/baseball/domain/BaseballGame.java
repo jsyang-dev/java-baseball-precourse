5349ed2ca67f6626fd61d4272127638e9bfe87b8
Jeongsu Yang <mycat83@gmail.com>
2021-10-03 17:35:59 +0900

    feat(baseballGame): 야구 게임 시작

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import nextstep.utils.Console;
+
 public class BaseballGame {
 
     private final BaseballNumbers baseballNumbersOfComputer;
@@ -14,5 +16,16 @@ public class BaseballGame {
 
     public BaseballNumbers getBaseballNumbersOfComputer() {
         return baseballNumbersOfComputer;
+    }
+
+    public void start() {
+        BaseballScore baseballScore;
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
+            BaseballNumbers baseballNumbersOfPlayer = BaseballNumbers.from(input);
+            baseballScore = baseballNumbersOfComputer.calculateScore(baseballNumbersOfPlayer);
+            System.out.println(baseballScore);
+        } while (!baseballScore.isWin());
     }
 }
