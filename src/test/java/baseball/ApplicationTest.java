4dd54c1034dffb87d0ce1caa567ff47372d7318c
Jeongsu Yang <mycat83@gmail.com>
2021-10-05 09:30:45 +0900

    test(applicationTest): 테스트 추가
    - 스트라이크, 볼 앱력 테스트
    - 잘못된 숫자 입력 테스트
    - 중복된 숫자 입력 테스트
    - 게임 종료 후 재시작 오입력 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -7,13 +7,46 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mockStatic;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 public class ApplicationTest extends NSTest {
     @BeforeEach
     void beforeEach() {
         super.setUp();
+    }
+
+    @Test
+    void 스트라이크() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(1, 3, 5);
+            running("136");
+            verify("2스트라이크");
+        }
+    }
+
+    @Test
+    void 볼() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(1, 3, 5);
+            running("567");
+            verify("1볼");
+        }
+    }
+
+    @Test
+    void 스트라이크_and_볼() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(1, 3, 5);
+            running("153");
+            verify("1스트라이크 2볼");
+        }
     }
 
     @Test
@@ -28,6 +61,28 @@ public class ApplicationTest extends NSTest {
     }
 
     @Test
+    void 잘못된_숫자_입력() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(7, 1, 3)
+                    .thenReturn(5, 8, 9);
+            running("023");
+            verify("[ERROR] 1부터 9사이의 숫자 3개");
+        }
+    }
+
+    @Test
+    void 중복된_숫자_입력() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(7, 1, 3)
+                    .thenReturn(5, 8, 9);
+            running("112");
+            verify("[ERROR] 중복되지 않은 숫자");
+        }
+    }
+
+    @Test
     void 게임종료_후_재시작() {
         try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
             mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
@@ -35,6 +90,16 @@ public class ApplicationTest extends NSTest {
                     .thenReturn(5, 8, 9);
             run("713", "1", "597", "589", "2");
             verify("3스트라이크", "게임 끝", "1스트라이크 1볼");
+        }
+    }
+
+    @Test
+    void 게임종료_후_재시작_오입력() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(7, 1, 3);
+            run("713", "3", "2");
+            verify("3스트라이크", "게임 끝", "[ERROR] 1 이나 2를 입력");
         }
     }
 
